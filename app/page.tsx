23a7efb521135aba7f755edb8cef792bfb802e80
Auggie <austin@thebennett.net>
2025-05-04 20:49:42 -0500

    edge runtime

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,8 @@ import ConnectSupabaseSteps from "@/components/tutorial/connect-supabase-steps";
 import SignUpUserSteps from "@/components/tutorial/sign-up-user-steps";
 import { hasEnvVars } from "@/utils/supabase/check-env-vars";
 
+export const runtime = 'edge';
+
 export default async function Home() {
   return (
     <>
