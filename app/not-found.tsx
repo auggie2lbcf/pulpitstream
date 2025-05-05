aece9c8b54731036d31aa12845322acf092216eb
Auggie <austin@thebennett.net>
2025-05-04 20:56:11 -0500

    not-found

---
+++ app/not-found.tsx
@@ -0,0 +1,19 @@
+// app/_not-found.js
+import Link from 'next/link';
+
+export const runtime = 'edge'; // Add this line
+
+export default function NotFound() {
+  return (
+    <div>
+      <h2>Not Found</h2>
+      <p>Could not find requested resource</p>
+      <p>
+        View <Link href="/protected">protected</Link>
+      </p>
+      <p>
+        View <Link href="/">Home</Link>
+      </p>
+    </div>
+  );
+}
